e19f0b526e48b1c8290627a4944b7de86d92be7c
Ikram Ali <mrikram1989@gmail.com>
2019-01-31 11:50:56 +0500

    [python] new function added for finding duplicates.

--- test_words.py
+++ test_words.py
@@ -1,5 +1,6 @@
 # coding: utf8
 """ Test cases """
+import pickle
 
 from urduhack.urdu_characters import URDU_ALPHABETS
 
@@ -13,3 +14,53 @@ def test_words():
         for character in word.strip():
             assert character in URDU_ALPHABETS, F"Incorrect word: {word} and char: {character}"
     handler.close()
+
+
+def sorted_words():
+    """Sorted the words.txt file"""
+    handler = open(file_name, encoding="utf8")
+    words_set = set()
+    for word in handler:
+        words_set.add(word.strip())
+    handler.close()
+    print(F"Total words: {len(words_set)}")
+
+    words_set = sorted(words_set)
+    with open(file_name, 'w', encoding="utf8") as the_file:
+        for word in words_set:
+            the_file.write(word + '\n')
+
+
+def duplicate_words():
+    """ find duplicate words"""
+    words_dict = {}
+    handler = open(file_name, encoding="utf8")
+    for word in handler:
+
+        inner_handler = open(file_name, encoding="utf8")
+        inner_list = []
+        for inner_word in inner_handler:
+            if word.strip() in inner_word.strip():
+                inner_list.append(inner_word.strip())
+
+        inner_handler.close()
+
+        if inner_list:
+            words_dict[word.strip()] = inner_list
+
+    handler.close()
+
+    with open('words.dict', 'wb') as fp:
+        pickle.dump(words_dict, fp)
+
+
+def load_pkl():
+    """Load the save dict file"""
+
+    with open('words.dict', 'rb') as fp:
+        word_dict = pickle.load(fp)
+
+    for key, values in word_dict.items():
+        print("=" * 50)
+        print(key)
+        print(values)
